f21937f519d2ce25683261c4278c2f7e373d2614
Nazmul1582 <nazmulhasan1582@gmail.com>
2025-10-16 19:47:32 +0600

    asynchronous task in JavaScript

---
+++ Module_05_Async_JS/async_code.js
@@ -0,0 +1,13 @@
+setTimeout(() => {
+  console.log("showing from setTimeout")
+})
+let count = 1
+do {
+  console.log(count)
+  count++
+} while (count <= 10)
+console.log("Hello, World!")
+
+// First, Hello, World! is printed.
+// then printed 1 to 10 from do while loop
+// then printed the text from setTimeout, though setTimeout is written first.
