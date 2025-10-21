95f340fcc77c0dc71f821b238b2f6924c1731aae
Nazmul1582 <nazmulhasan1582@gmail.com>
2025-10-21 08:34:33 +0600

    solution of callback hell using promise

--- Module_05_Async_JS/promise.js
+++ Module_05_Async_JS/promise.js
@@ -8,3 +8,36 @@ let promise = new Promise((resolve, reject) => {
   }
 })
 promise.then((res) => console.log(res)).catch((err) => console.log(err))
+
+function getUser() {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve("👤 User fetched"), 1000)
+  })
+}
+
+function getPosts() {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve("📝 Posts fetched"), 1000)
+  })
+}
+
+function getComments() {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve("💬 Comments fetched"), 1000)
+  })
+}
+
+getUser()
+  .then((result) => {
+    console.log(result)
+    return getPosts()
+  })
+  .then((result) => {
+    console.log(result)
+    return getComments()
+  })
+  .then((result) => {
+    console.log(result)
+    console.log("✅ All data fetched!")
+  })
+  .catch((err) => console.error("Error:", err))
