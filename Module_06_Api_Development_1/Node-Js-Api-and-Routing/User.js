14557d820c43d0140036a0cedb0ee989f94ed572
Nazmul1582 <nazmulhasan1582@gmail.com>
2025-10-29 06:46:57 +0600

    created a post request

--- Module_06_Api_Development_1/Node-Js-Api-and-Routing/User.js
+++ Module_06_Api_Development_1/Node-Js-Api-and-Routing/User.js
@@ -5,4 +5,22 @@ router.get("/user/:id", (req, res) => {
   res.status(200).send("Welcome!")
 })
 
+router.post("/user/create", (req, res) => {
+  console.log(req.body)
+  const { name, age } = req.body
+  if (!name) {
+    res.status(400).send({
+      message: "Name is required!",
+    })
+  }
+  const user = {
+    name,
+    age,
+  }
+  res.status(201).json({
+    message: "User created successfully",
+    user,
+  })
+})
+
 module.exports = router
