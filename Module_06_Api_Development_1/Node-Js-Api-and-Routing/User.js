661b56c83ad98dfbb43ca22cbd10b44233519d45
Nazmul1582 <nazmulhasan1582@gmail.com>
2025-10-29 07:23:45 +0600

    create a post api using data modeling by class

--- Module_06_Api_Development_1/Node-Js-Api-and-Routing/User.js
+++ Module_06_Api_Development_1/Node-Js-Api-and-Routing/User.js
@@ -1,21 +1,25 @@
 const express = require("express")
 const router = express.Router()
 
+// Data Modeling
+class User {
+  constructor({ name, age, address }) {
+    this.name = name
+    this.age = age
+    this.address = address
+  }
+}
+
 router.get("/user/:id", (req, res) => {
   res.status(200).send("Welcome!")
 })
 
 router.post("/user/create", (req, res) => {
-  console.log(req.body)
-  const { name, age } = req.body
-  if (!name) {
+  const user = new User(req.body)
+  if (!user.name) {
     res.status(400).send({
       message: "Name is required!",
     })
-  }
-  const user = {
-    name,
-    age,
   }
   res.status(201).json({
     message: "User created successfully",
