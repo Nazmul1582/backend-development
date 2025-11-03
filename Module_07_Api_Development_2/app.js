b50f808f394c4ad9e6e8cc9f9efe0599bd078849
Nazmul1582 <nazmulhasan1582@gmail.com>
2025-11-03 08:51:48 +0600

    implemented the rate limit features by using express-rate-limit

--- Module_07_Api_Development_2/app.js
+++ Module_07_Api_Development_2/app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+const { rateLimit } = require("express-rate-limit")
 const port = process.env.PORT || 3000
 
 const userRoute = require("./routes/userRoute")
@@ -7,7 +8,17 @@ const { checkUserAgent, isValidUser } = require("./middleware/user")
 
 app.use(express.json())
 app.use(checkUserAgent)
-app.use("/api/users", isValidUser, userRoute)
+
+const rateLimiter = rateLimit({
+  windowMs: 10 * 1000,
+  max: 10,
+  message: {
+    message: "Too many requests from this ip. Please try again in 30 seconds",
+  },
+})
+
+// use the users route
+app.use("/api/users", rateLimiter, isValidUser, userRoute)
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`)
