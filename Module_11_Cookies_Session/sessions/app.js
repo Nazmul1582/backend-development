1187b783e1c3a946d764236478b2e554cfe40e64
Nazmul1582 <nazmulhasan1582@gmail.com>
2025-12-18 23:26:37 +0600

    set and get cookie using cookie-parser

--- Module_11_Cookies_Session/sessions/app.js
+++ Module_11_Cookies_Session/sessions/app.js
@@ -1,9 +1,22 @@
 const express = require("express")
+const cookieParser = require("cookie-parser")
 const app = express()
+
+app.use(cookieParser())
 
 app.get("/set-cookie", (req, res) => {
   res.cookie("name", "nazmul", { httpOnly: true })
   res.send("Cookie has been set successfully!")
+})
+
+app.get("/get-cookie", (req, res) => {
+  const name = req.cookies.name
+
+  if (name === "nazmul") {
+    res.send(`Cookie received: ${name}`)
+  } else {
+    res.send("Unauthorized!")
+  }
 })
 
 app.listen(3000, () => {
