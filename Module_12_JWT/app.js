c7612abdaf0b5e16ca2d6551738d2ba55fd16061
Nazmul1582 <nazmulhasan1582@gmail.com>
2026-01-03 16:43:04 +0600

    created a protected route and check the authorization with hash value

--- Module_12_JWT/app.js
+++ Module_12_JWT/app.js
@@ -17,6 +17,18 @@ app.post("/signin", (req, res) => {
   res.send(`hashed value: ${hash}`)
 })
 
+app.get("/protected", (req, res) => {
+  const { username, password, hash } = req.query
+  const data = JSON.stringify({ username, password })
+  const newHash = crypto
+    .createHmac("sha256", "asd5r34u85iu348")
+    .update(data)
+    .digest("hex")
+
+  if (hash !== newHash) return res.status(401).send("Unauthorized!")
+  return res.send("Your are authorized to access this protected route")
+})
+
 app.listen(3000, () => {
   console.log("Server is runnig on http://localhost:3000")
 })
